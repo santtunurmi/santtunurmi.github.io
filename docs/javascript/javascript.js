f7fa9fdd1b5e1858013084147887084c94046f80
Santtu Nurmi <AC1244@student.jamk.fi>
2025-05-20 18:18:11 +0300

    page navigation is now operational

--- docs/javascript/javascript.js
+++ docs/javascript/javascript.js
@@ -1,9 +1,21 @@
 function navEventListener() {
-    document.querySelectorAll("nav a").forEach(x => x.addEeventListener("click", _ => JS_PageLoading(x.dataset.id)))
+    document.querySelectorAll("nav a").forEach(nav_row => nav_row.addEventListener("click", _ => {JS_NavLoading(nav_row.dataset.id); JS_PageLoading(nav_row.dataset.id)}));
 };
 
-function JS_PageLoading() {
-    let pages = document.querySelectorAll("div article")
-    pages.forEach(y => y.classList.remove("active"))
-    pages[n-1].classList.add("active")
-}
+function JS_NavLoading(nav_id) {
+    let pages = document.querySelectorAll("div.active, div.inactive");
+    console.log(pages);
+    pages.forEach(page => {page.classList.remove("active"); page.classList.add("inactive")});
+    pages[nav_id - 1].classList.remove("inactive");
+    pages[nav_id - 1].classList.add("active")
+};
+
+function JS_PageLoading(nav_id) {
+    let pages = document.querySelectorAll("article.active, article.inactive");
+    console.log(pages);
+    pages.forEach(page => {page.classList.remove("active"); page.classList.add("inactive")});
+    pages[nav_id - 1].classList.remove("inactive");
+    pages[nav_id - 1].classList.add("active")
+};
+
+navEventListener();
