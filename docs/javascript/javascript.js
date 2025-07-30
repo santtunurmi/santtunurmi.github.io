7281afb61fcaac969065a94c12142396bcca5107
Santtu Nurmi <AC1244@student.jamk.fi>
2025-07-30 18:51:00 +0300

    read more button added on portfolio screen when screen width less than 600px

--- docs/javascript/javascript.js
+++ docs/javascript/javascript.js
@@ -10,22 +10,60 @@
 
     function JS_NavLoading(nav_id) {
         let pages = document.querySelectorAll("div.active, div.inactive");
-        console.log(pages);
         pages.forEach(page => {page.classList.remove("active"); page.classList.add("inactive")});
         pages[nav_id - 1].classList.remove("inactive");
-        pages[nav_id - 1].classList.add("active")
+        pages[nav_id - 1].classList.add("active");
     };
 
     /* Handles which page should be active on the site. */
 
     function JS_PageLoading(nav_id) {
         let pages = document.querySelectorAll("article.active, article.inactive");
-        console.log(pages);
         pages.forEach(page => {page.classList.remove("active"); page.classList.add("inactive")});
         pages[nav_id - 1].classList.remove("inactive");
-        pages[nav_id - 1].classList.add("active")
+        pages[nav_id - 1].classList.add("active");
     };
 
-    /* The script is ran by default. */
+/* Script for setting up "read more" -pages when viewport is less than 600px. */
 
-    navEventListener();
+    function buttonReadMore(checks, fullText) {
+        document.querySelectorAll("article.read-more")[checks].innerHTML = fullText;
+        document.querySelectorAll("button.read-more-button")[checks].classList.add("inactive");
+        document.querySelectorAll("button.read-more-button")[checks].classList.remove("active");
+        checks += 1
+    }
+
+    function ReadMoreAdd() {
+        let ReadMores = document.querySelectorAll("article.read-more");
+        let shortText;
+        checks = ReadMores.length - 1
+        const shortTextLength = 231;
+        ReadMores.forEach(Readmore => {
+            fullText = Readmore.innerHTML;
+            shortText = Readmore.innerHTML.substr(0, shortTextLength) + "... </p>" +
+            "<button class='read-more-button'>Show more</button>";
+            Readmore.innerHTML = shortText;
+            document.querySelectorAll("button.read-more-button").forEach(button => button.addEventListener("click", _ => {buttonReadMore(checks, fullText)}));
+        });
+    };
+
+    function ReadMoreRemove() {
+        document.querySelectorAll("article.read-more").forEach(Readmore => {try{Readmore.innerHTML = fullText} catch{""}})
+        document.querySelectorAll("button.read-more-button").forEach(page => {page.classList.remove("active"); page.classList.add("inactive")});
+    };
+
+    /* Viewport monitoring */
+
+    if (matchMedia) {
+        var mq = window.matchMedia("(max-width: 600px)");
+        mq.addListener(WidthChange);
+    }
+
+    function WidthChange(mq) {
+        if (mq.matches) { 
+            ReadMoreAdd();
+        } 
+        else { 
+            ReadMoreRemove();
+        }
+    }
